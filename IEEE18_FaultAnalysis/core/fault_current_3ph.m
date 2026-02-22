abafdadf5ee9de927549ab90abc88bf8f5000a17
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2026-02-22 20:33:51 +0530

    dtu

--- IEEE18_FaultAnalysis/core/fault_current_3ph.m
+++ IEEE18_FaultAnalysis/core/fault_current_3ph.m
@@ -1,3 +1,7 @@
+## @brief Compute three-phase fault current
+## @param Vprefault Prefault voltage at the bus
+## @param Z1 Positive sequence impedance
+## @return Fault current in per-unit
 function If = fault_current_3ph(Vprefault, Z1)
     % Three-phase fault current
     If = Vprefault / Z1;
