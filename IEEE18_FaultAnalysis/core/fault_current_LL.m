9b364888b69656c29f16d1149a8033fa865490d6
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2026-02-22 21:54:30 +0530

    FHY

--- IEEE18_FaultAnalysis/core/fault_current_LL.m
+++ IEEE18_FaultAnalysis/core/fault_current_LL.m
@@ -1,3 +1,12 @@
+/**
+** @brief Compute line-to-line fault current
+**
+** @param Vprefault Prefault voltage at the bus
+** @param Z1 Positive sequence impedance
+** @param Z2 Negative sequence impedance
+** @return Fault current in per-unit
+*/
+
 function If = fault_current_LL(Vprefault, Z1, Z2)
     % Line-to-Line fault current
     If = sqrt(3)*Vprefault / (Z1 + Z2);
