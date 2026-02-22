ee8256d3c0dd2350d9e56036ae1f45d171617d4c
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2026-02-22 22:01:08 +0530

    fhdry

--- IEEE18_FaultAnalysis/core/inv_symmetrical.m
+++ IEEE18_FaultAnalysis/core/inv_symmetrical.m
@@ -1,3 +1,11 @@
+/**
+** @brief Inverse symmetrical component transformation
+**
+** @param V0 Zero sequence voltage
+** @param V1 Positive sequence voltage
+** @param V2 Negative sequence voltage
+** @return Phase voltages Va,Vb,Vc
+*/
 function Vabc = inv_symmetrical(V0, V1, V2)
     % Inverse transform: V0,V1,V2 -> Va,Vb,Vc
     a = exp(1j*2*pi/3);
