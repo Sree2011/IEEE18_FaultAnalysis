97a7e92c4ed588662f3213d25cbeb301057562cd
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2026-02-22 21:51:25 +0530

    YFTH

--- IEEE18_FaultAnalysis/core/fault_current_DLG.m
+++ IEEE18_FaultAnalysis/core/fault_current_DLG.m
@@ -1,4 +1,4 @@
-%{
+
 /*****************************************************************
  * @file fault_current_DLG.m
  * @brief Compute double line-to-ground fault current
@@ -8,8 +8,7 @@
  * @param Z1 Positive sequence impedance
  * @param Z2 Negative sequence impedance
  * @return Fault current in per-unit
- *****************************************************
- /%}
+ *****************************************************/
 
 function If = fault_current_DLG(Vprefault, Z0, Z1, Z2)
     % Double Line-to-Ground fault current
