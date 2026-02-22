272519b7604f6513820ebb023ee568a223a107d6
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2026-02-22 20:41:04 +0530

    GUR

--- IEEE18_FaultAnalysis/core/fault_current_SLG.m
+++ IEEE18_FaultAnalysis/core/fault_current_SLG.m
@@ -1,3 +1,4 @@
+%{
 ## @brief Compute single line-to-ground fault current
 ##
 ## @param Vprefault Prefault voltage at the bus
@@ -5,6 +6,7 @@
 ## @param Z1 Positive sequence impedance
 ## @param Z2 Negative sequence impedance
 ## @return Fault current in per-unit
+%}
 function If = fault_current_SLG(Vprefault, Z0, Z1, Z2)
     % Single Line-to-Ground fault current
     If = 3*Vprefault / (Z0 + Z1 + Z2);
