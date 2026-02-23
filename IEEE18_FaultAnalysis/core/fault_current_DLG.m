74881464c9465f871fb00755438ec64ab9307ab4
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2026-02-23 13:18:06 +0530

    gh,

--- IEEE18_FaultAnalysis/core/fault_current_DLG.m
+++ IEEE18_FaultAnalysis/core/fault_current_DLG.m
@@ -1,12 +1,5 @@
 
-## @file fault_current_DLG.m
-## @brief Compute double line-to-ground fault current
-##
-## @param Vprefault Prefault voltage at the bus
-## @param Z0 Zero sequence impedance
-## @param Z1 Positive sequence impedance
-## @param Z2 Negative sequence impedance
-## @return Fault current in per-unit
+
 function If = fault_current_DLG(Vprefault, Z0, Z1, Z2)
     % Double Line-to-Ground fault current
     Zeq = (Z0*(Z1+Z2)) / (Z0 + Z1 + Z2);  % equivalent impedance
--- IEEE18_FaultAnalysis/core/fault_current_SLG.m
+++ IEEE18_FaultAnalysis/core/fault_current_SLG.m
@@ -1,15 +1,4 @@
-## -*- texinfo -*-
-## @deftypefn {Function File} {@var{If} =} fault_current_SLG (@var{Vprefault}, @var{Z0}, @var{Z1}, @var{Z2})
-## @brief Single line-to-ground fault current calculation
-##
-## Computes the fault current for SLG faults using sequence impedances.
-##
-## @param Vprefault Prefault voltage at the bus
-## @param Z0 Zero sequence impedance
-## @param Z1 Positive sequence impedance
-## @param Z2 Negative sequence impedance
-## @return Fault current in per-unit
-## @end deftypefn
+
 function If = fault_current_SLG(Vprefault, Z0, Z1, Z2)
     % Single Line-to-Ground fault current
     If = 3*Vprefault / (Z0 + Z1 + Z2);
