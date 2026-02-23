f393777475f7657214ae29dca3aad7323ddf333b
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2026-02-23 12:17:36 +0530

    vxh

--- IEEE18_FaultAnalysis/core/fault_current_3ph.m
+++ IEEE18_FaultAnalysis/core/fault_current_3ph.m
@@ -1,16 +1,31 @@
-## --*texinfo*--
-# NAME
-# fault_current_3ph - Calculate three-phase fault current
-# SYNOPSIS
-# If = fault_current_3ph(Vprefault, Z1)
-# DESCRIPTION
-# This function calculates the three-phase fault current (If) based on the prefault voltage (Vprefault) and the positive sequence impedance (Z1) of the system.
-# INPUT
-# - Vprefault: The prefault voltage at the point of fault (in volts).
-# - Z1: The positive sequence impedance of the system (in ohms).
-# OUTPUT
-# - If: The three-phase fault current (in amperes).
-# @end texinfo
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{If} =} fault_current_3ph (@var{Vprefault}, @var{Z1})
+## @section Description
+## Calculates the three-phase fault current (@var{If}) based on
+## prefault voltage (@var{Vprefault}) and positive sequence impedance (@var{Z1}).
+##
+## @subsection Inputs
+## @itemize
+##   @item @var{Vprefault}: Prefault voltage (p.u.)
+##   @item @var{Z1}: Positive sequence impedance (p.u.)
+## @end itemize
+##
+## @subsection Output
+## @itemize
+##   @item @var{If}: Three-phase fault current (p.u.)
+## @end itemize
+##
+## @example
+## Vprefault = 1.0;
+## Z1 = 0.1;
+## If = fault_current_3ph(Vprefault, Z1);
+## disp(['The three-phase fault current is: ', num2str(If), ' p.u.']);
+## @end example
+##
+## @end deftypefn
+function If = fault_current_3ph(Vprefault, Z1)
+    If = Vprefault / Z1;
+end
 function If = fault_current_3ph(Vprefault, Z1)
     % Three-phase fault current
     If = Vprefault / Z1;
